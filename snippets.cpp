8c97987db77295e8c8c07d14bc2627a18ef7607e
asifebrahim <jnesa38@gmail.com>
2025-01-17 23:15:02 +0530

    Added the priority queue

---
+++ snippets.cpp
@@ -0,0 +1,19 @@
+priority_queue<int,vector<int>,grater<>> pq;
+
+for min heap as in c++ normal priority queue is max heap 
+
+so we use min heap by using grater as the comparator.
+
+We can also use decltype in that case
+
+auto compare=[](const int &a,const int &b){
+    return a>b;
+}
+priority_queue<int,vector<int>,decltype(compare)> pq;
+
+its more like a custom comparator
+
+in pq the return acts kinda opposite to normal comparators return type.
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+
